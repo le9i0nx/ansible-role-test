50c2e9c710f6df0b2fe417ee877860568aa8e07a
Gavrilov Aleksej <le9i0nx@gmail.com>
2015-12-01 11:39:55 +0500

    bin del threading

--- bin/virt.py
+++ bin/virt.py
@@ -4,7 +4,6 @@ import yaml
 import subprocess
 import os
 import sys
-import threading
 
 def proc(cmd,sh = True ):
     p = subprocess.Popen(cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE, shell=sh)
@@ -20,7 +19,7 @@ def job(dockerf, dis , ver):
     o = proc("docker build -f {} -t {}_{} .".format(dockerf,dis,ver))
     o1_cmd = "docker run -d --cap-add=SYS_ADMIN -it -v /sys/fs/cgroup:/sys/fs/cgroup:ro {}_{}".format(dis,ver)
     o1 = proc("docker run -d --cap-add=SYS_ADMIN -it -v /sys/fs/cgroup:/sys/fs/cgroup:ro {}_{}".format(dis,ver))
-    print("$ {}\n{}\n$ {}\n{}".format(o_cmd,o[0],o1_cmd,o1[0]))
+    print("$    {}\n{}\n$   {}\n{}".format(o_cmd,o[0],o1_cmd,o1[0]))
     return
 
 ROOT_PATH=os.path.dirname(__file__)
@@ -35,7 +34,7 @@ cmd_list = [
 
 for item in cmd_list:
     out = proc(item)
-    print("$ {}\n{}".format(item,out[0]))
+    print("$    {}\n{}".format(item,out[0]))
 
 with open('meta/main.yml', 'r') as f:
     doc = yaml.load(f)
@@ -45,14 +44,10 @@ for i in doc["galaxy_info"]["platforms"]:
     for x in i["versions"]:
         dockerfile = "{}/../dockerfile/{}/{}/Dockerfile".format(ROOT_PATH,distrib,x)
         if os.path.exists(dockerfile):
-            threading.Thread(target=job, args=( dockerfile, distrib, x)).start()
+            job(dockerfile, distrib, x)
         else:
             print("Critical error. Not found docker files {}".format(dockerfile))
             sys.exit(1)
-
-for th in threading.enumerate():
-    if th != threading.currentThread():
-        th.join()
 
 cmd_list =[
     "sleep 10",
